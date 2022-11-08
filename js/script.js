3070ae99c70de0ae44da84e30e09dfb1c9af3f6e
Michael Szewczyk <mszewczyk@hawk.iit.edu>
2022-11-07 22:39:30 -0600

    Commit 2
    Added js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  function updateText() {
+	  var userInput = document.getElementById('entryinput').value;
+	  document.getElementById("textoutput").innerText = userInput;
+	  alert(Michael Szewczyk: " + userInput);
+  }
+  
+  var button = document.getElementById("entrybutton");
+  button.addEventListener("click",updateText);
+}
 window.addEventListener('load', init);
